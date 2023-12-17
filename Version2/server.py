adc27dd47326757e504ac71421b4938e450a2c69
VuHuy <huyv80313@gmail.com>
2023-12-17 10:53:40 +0700

    update version 2

--- Version2/server.py
+++ Version2/server.py
@@ -1,30 +1,75 @@
 import tkinter as tk
 from tkinter import ttk, scrolledtext
-from datetime import datetime
-import socket
-import threading
-import os
 from PIL import Image, ImageTk
+from pymongo import MongoClient
 import random
-import pandas as pd
+import pandas as pd 
 from NaiveBayes import NaiveBayes  
+import os 
 
-class GmailServer:
+class GmailApp:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("Gmail Server")
-        self.root.geometry("800x600")
+        self.root.title("Gmail App")
+        self.root.geometry("800x700")
 
         self.ham_messages = []
         self.spam_messages = []
 
-        self.message_index = 1  # Thêm dòng này để khởi tạo self.message_index
+        self.message_index = 1
 
         self.create_gui()
 
-        threading.Thread(target=self.start_server).start()
-        
+        self.load_messages_from_database()
 
+    def train_spam_classifier(self):
+        # Đọc dữ liệu huấn luyện từ file CSV hoặc cơ sở dữ liệu
+        # sms_spam = pd.read_csv('SMSSpamCollection', sep='\t', header=None, names=['Label', 'SMS'])
+
+        # # Randomize the dataset
+        # data_randomized = sms_spam.sample(frac=1, random_state=1)
+        # # Calculate index for split
+        # training_test_index = round(len(data_randomized) * 0.8)
+        # # Split into training and test sets
+        # training_set = data_randomized[:training_test_index].reset_index(drop=True)
+        
+        sms_spam = pd.read_csv('SMSSpamCollection', sep='\t', header=None, names=['Label', 'SMS'])
+        data_randomized = sms_spam.sample(frac=1, random_state=1)
+        training_test_index = round(len(data_randomized) * 0.8)
+        training_set = data_randomized[:training_test_index].reset_index(drop=True)
+
+        spam_classifier = NaiveBayes(alpha=1)
+        # spam_classifier.train(training_set)
+
+        # Huấn luyện mô hình
+        self.spam_classifier.train(training_set)
+
+    def load_messages_from_database(self):
+        # Kết nối đến MongoDB
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['email']
+        email_collection = db['email_messages']
+
+        # Tải tin nhắn từ cơ sở dữ liệu
+        messages = email_collection.find()
+        for message in messages:
+            sender = message.get('sender', 'Unknown Sender')
+            subject = message.get('subject', 'No Subject')
+            date = message.get('date', 'Unknown Date')
+            body_text = message.get('body_text', 'No Body Text')
+
+            # Phân loại tin nhắn là Ham hay Spam
+            is_spam = self.spam_classifier.classify(body_text)
+
+            if is_spam:
+                self.spam_messages.append((sender, subject, date, body_text))
+                self.spam_listbox.insert(tk.END, f"{self.message_index}. {sender} - {subject}")
+            else:
+                self.ham_messages.append((sender, subject, date, body_text))
+                self.ham_listbox.insert(tk.END, f"{self.message_index}. {sender} - {subject}")
+
+            self.message_index += 1
+    
     def create_gui(self):
         inbox_frame = ttk.Frame(self.root, padding=(10, 10, 10, 10))
         inbox_frame.grid(row=0, column=0, sticky="nsew")
@@ -72,60 +117,70 @@ class GmailServer:
         email_frame.columnconfigure(0, weight=1)
         email_frame.rowconfigure(1, weight=1)
 
-    def start_server(self):
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind(('0.0.0.0', 5555))
-        server_socket.listen(5)
+    def load_messages_from_database(self):
+        # Kết nối đến MongoDB
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['email']  # Thay 'email' bằng tên thực tế của cơ sở dữ liệu MongoDB
+        email_collection = db['email']
 
-        while True:
-            client, addr = server_socket.accept()
-            threading.Thread(target=self.receive_messages, args=(client,)).start()
+        # Tải tin nhắn từ cơ sở dữ liệu
+        messages = email_collection.find()
+        for message in messages:
+            sender = message.get('sender', 'Unknown Sender')
+            subject = message.get('subject', 'No Subject')
+            date = message.get('date', 'Unknown Date')
+            body_text = message.get('body_text', 'No Body Text')
+            is_spam = message.get('is_spam', False)
 
-    def receive_messages(self, client):
-        try:
-            data = client.recv(1024)
-            if not data:
-                return
-            timestamp = datetime.now().strftime("%H:%M:%S")
             avatar_path = self.get_random_avatar_path()
             avatar_image = self.load_avatar_image(avatar_path)
-            message_content = data.decode('utf-8')
 
-            # Phân loại tin nhắn thành Ham hoặc Spam
-            result = spam_classifier.classify(message_content)
-            if result == False:
-                # Phân loại là Spam3
-                print(f"Predicted: Spam - Actual: {message_content}")
-                message = f"{self.message_index}. {timestamp} - Client (Spam): {message_content}\n"
-                self.spam_messages.append((message, avatar_image))
-                self.spam_listbox.insert(tk.END, f"{self.message_index}. Client - {timestamp}")
+            if is_spam:
+                self.spam_messages.append((sender, subject, date, body_text, avatar_image))
+                self.spam_listbox.insert(tk.END, f"{self.message_index}. {sender} - {subject}")
             else:
-                print(f"Predicted: Ham - Actual: {message_content}")
-                message = f"{self.message_index}. {timestamp} - Client (Ham): {message_content}\n"
-                self.ham_messages.append((message, avatar_image))
-                self.ham_listbox.insert(tk.END, f"{self.message_index}. Client - {timestamp}")
+                self.ham_messages.append((sender, subject, date, body_text, avatar_image))
+                self.ham_listbox.insert(tk.END, f"{self.message_index}. {sender} - {subject}")
 
             self.message_index += 1
-            self.root.update_idletasks()
-        except ConnectionResetError:
-            pass
 
+    def display_email(self, message):
+        self.email_display_text.config(state=tk.NORMAL)
+        self.email_display_text.delete(1.0, tk.END)
 
+        sender_text = f"Sender: {message[0]}\n"
+        subject_text = f"Subject: {message[1]}\n"
+        date_text = f"Date: {message[2]}\n"
+        body_text = f"Body Text:\n{message[3]}\n"
+
+        self.email_display_text.insert(tk.END, sender_text)
+        self.email_display_text.insert(tk.END, subject_text)
+        self.email_display_text.insert(tk.END, date_text)
+        self.email_display_text.insert(tk.END, body_text)
+
+        # Hiển thị avatar
+        avatar_image = message[4]
+        if avatar_image:
+            avatar_label = tk.Label(self.email_display_frame, image=avatar_image, borderwidth=2, relief="solid", width=100, height=100)
+            avatar_label.grid(row=4, column=0, padx=(0, 10), sticky="w")
+
+        self.email_display_text.config(state=tk.DISABLED)
+    
     def display_selected_message(self, event):
         selected_ham_index = self.ham_listbox.curselection()
         selected_spam_index = self.spam_listbox.curselection()
 
         if selected_ham_index:
             selected_index = int(selected_ham_index[0])
-            selected_message, selected_avatar = self.ham_messages[selected_index]
+            selected_message = self.ham_messages[selected_index]
         elif selected_spam_index:
             selected_index = int(selected_spam_index[0])
-            selected_message, selected_avatar = self.spam_messages[selected_index]
+            selected_message = self.spam_messages[selected_index]
         else:
             return
 
-        self.display_email(selected_message, selected_avatar)
-
+        self.display_email(selected_message)
+    
     def delete_selected_message(self, inbox_type):
         selected_ham_index = self.ham_listbox.curselection()
         selected_spam_index = self.spam_listbox.curselection()
@@ -139,17 +194,21 @@ class GmailServer:
             self.spam_listbox.delete(selected_index)
             del self.spam_messages[selected_index]
 
-    def display_email(self, message, avatar):
-        for widget in self.email_display_frame.winfo_children():
-            widget.destroy()
+    def display_email(self, message):
+        self.email_display_text.config(state=tk.NORMAL)
+        self.email_display_text.delete(1.0, tk.END)
 
-        avatar_label = tk.Label(self.email_display_frame, image=avatar, borderwidth=2, relief="solid", width=100, height=100)
-        avatar_label.grid(row=0, column=0, padx=(0, 10), sticky="w")
+        sender_text = f"Sender: {message[0]}\n"
+        subject_text = f"Subject: {message[1]}\n"
+        date_text = f"Date: {message[2]}\n"
+        body_text = f"Body Text:\n{message[3]}\n"
 
-        text_widget = scrolledtext.ScrolledText(self.email_display_frame, wrap=tk.WORD, width=60, height=8, font=("Helvetica", 12))
-        text_widget.grid(row=0, column=1, sticky="nsew")
-        text_widget.insert(tk.END, message)
-        text_widget.config(state=tk.DISABLED)
+        self.email_display_text.insert(tk.END, sender_text)
+        self.email_display_text.insert(tk.END, subject_text)
+        self.email_display_text.insert(tk.END, date_text)
+        self.email_display_text.insert(tk.END, body_text)
+
+        self.email_display_text.config(state=tk.DISABLED)
 
     def get_random_avatar_path(self):
         avatar_dir = "Image"
@@ -165,18 +224,10 @@ class GmailServer:
             img_photo = ImageTk.PhotoImage(img)
             return img_photo
         return None
-
+    
     def run(self):
         self.root.mainloop()
 
 if __name__ == "__main__":
-    sms_spam = pd.read_csv('SMSSpamCollection', sep='\t', header=None, names=['Label', 'SMS'])
-    data_randomized = sms_spam.sample(frac=1, random_state=1)
-    training_test_index = round(len(data_randomized) * 0.8)
-    training_set = data_randomized[:training_test_index].reset_index(drop=True)
-
-    spam_classifier = NaiveBayes(alpha=1)
-    spam_classifier.train(training_set)
-
-    gmail_server = GmailServer()
-    gmail_server.run()
+    gmail_app = GmailApp()
+    gmail_app.run()
